715e41ea1aec1047eec2e7a1e475b2e83e4a4044
Joshua Bourgeot <dragonsploder@gmail.com>
2022-10-15 05:02:40 -0400

    For orestes

--- src/jrombk.h
+++ src/jrombk.h
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 extern clock_t frameStart;
 
--- src/main.c
+++ src/main.c
@@ -1,10 +1,32 @@
 #include "jrombk.h"
+#include "network.h"
+#include <curses.h>
 
 #define MS_PER_FRAME (CLOCKS_PER_SEC / 60)
 
 clock_t frameStart;
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("Invalid Arguments\n");
+        return 1;
+    }
+
+    bool isServer = true;
+    ServerConnection server;
+    ClientConnection connection1;
+
+    if (strcmp(argv[1], "-S") == 0) {
+        initServer(&server);
+        isServer = true;
+    } else if (strcmp(argv[1], "-C") == 0) {
+        initClient(&connection1, argv[2]);
+        isServer = false;
+    } else {
+        return 1;
+    }
+
+    // Init network and wait for connect
 
     createPlayer(&player1, 100, COLOR_RED, MAP_WIDTH/2 - MAP_RADIUS, MAP_HEIGHT / 2);
     createPlayer(&player2, 100, COLOR_BLUE, MAP_WIDTH/2 + MAP_RADIUS, MAP_HEIGHT / 2);
@@ -16,20 +38,40 @@ int main() {
     generateMap();
 
 
-    int command = 0;
-    while (command != 'q') {
-        frameStart = clock();
-        command = getch();
-        if (command == 'r') {
-            generateMap();
-        }
-        gameLoop(command, 0);
-        printScreen();
+    int command0 = 0;
+    int command1 = 0;
+    int command2 = 0;
+    if (isServer) {
+        while (command0 != 'q') {
+            frameStart = clock();
+            command0 = getch();
 
-        clock_t frameLen = 0;
-        do {
-            frameLen = clock() - frameStart; 
-        } while (frameLen < MS_PER_FRAME);
+            command1 = networkGetch(&server);
+
+            if (command0 == 'r') {
+                generateMap();
+            }
+
+            gameLoop(command1, command2);
+            printScreen();
+
+            mvprintw(0,0, "%i ", command1);
+
+            clock_t frameLen = 0;
+            do {
+                frameLen = clock() - frameStart; 
+            } while (frameLen < MS_PER_FRAME);
+        }
+        closeServer(&server);
+    } else {
+        while (command1 != 'q') {
+            do {
+                command1 = getch();
+            } while (command1 == ERR);
+           
+            clientPut(&connection1, command1);
+        }
+        closeClient(&connection1);
     }
 
     endwin();
