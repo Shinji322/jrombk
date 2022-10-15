a0dd910315a96c47ce3757e52b5d0528f5cd28f7
Ronan Maharaj <ronananakinmaharaj@gmail.com>
2022-10-14 20:33:40 -0400

    Wrote the player struct

--- src/game.c
+++ src/game.c
@@ -1,3 +1,13 @@
 #include "jrombk.h"
+#define SPRITE '#'
+
+typedef struct Player {
+    char sprite;
+    float health;
+    float speed;
+} Player;
 
 
+Player createPlayer(float health, float speed) {
+    return (Player){ SPRITE, health, speed };
+}
